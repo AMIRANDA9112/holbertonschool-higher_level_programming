66a104b5700fc18135b990323f0b75b1c95fe42a
AMIRANDA9112 <andreselfm@gmail.com>
2020-04-30 22:16:14 -0500

    line limit againt

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,8 @@ if number < 0:
 else:
     ld = (number % 10)
 if ld > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, ld))
+    print("Last digit of {:d} is {:d} ".format(number, ld) +
+          "and is greater than 5")
 elif ld < 6:
     if ld == 0:
         print("Last digit of {:d} is {:d} and is 0".format(number, ld))
