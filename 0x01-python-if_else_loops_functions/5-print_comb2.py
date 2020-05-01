40c3ef429a99474d932232e8476dcafcdf3851ed
AMIRANDA9112 <andreselfm@gmail.com>
2020-04-30 22:59:44 -0500

    change strategy

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for n in range(0, 10):
-    for i in range(0, 10):
-        if i == 9 and n == 9:
-            print("{:d}{:d}".format(n, i), end="")
-        else:
-            print("{:d}{:d}, ".format(n, i), end="")
+for i in range(0, 100):
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))
