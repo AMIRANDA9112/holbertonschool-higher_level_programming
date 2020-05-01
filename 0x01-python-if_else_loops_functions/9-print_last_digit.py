36508ad3025fa6d342abc4221729d4acfc7cd229
AMIRANDA9112 <andreselfm@gmail.com>
2020-04-30 23:59:36 -0500

    print last number

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        ln = (number * -1) % 10
+    else:
+        ln = number % 10
+    print("{:d}".format(ln), end="")
+    return ln
+
