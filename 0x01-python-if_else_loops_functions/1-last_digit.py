714d36f3f81326ca839f8e5704b5ebfbc33730a9
AMIRANDA9112 <andreselfm@gmail.com>
2020-04-30 21:59:45 -0500

    conditional sort

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,11 @@ if number < 0:
     ld = ((number * -1) % 10)
 else:
     ld = (number % 10)
-if ld != 0 and ld < 6:
-    print("Last digit of {} is {} ".format(number, ld) +
-          "and is less than 6 and not 0")
+if ld == 0:
+    print("Last digit of {} is {} and is 0".format(number, ld))
 elif ld > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, ld))
 else:
-    print("Last digit of {} is {} and is 0".format(number, ld))
+    print("Last digit of {} is {} ".format(number, ld) +
+          "and is less than 6 and not 0")
+
