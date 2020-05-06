9a8aed5d7a0306c6d503b2f09cafc91e654d35e4
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-05 23:02:36 -0500

    add in new list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    la_list = my_list
+    for i in la_list:
+        continue
+    if idx < 0 or idx >= i:
+        return my_list
+    else:
+        la_list[idx] = element
+        return la_list
