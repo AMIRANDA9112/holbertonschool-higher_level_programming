57d2367c9a141861af4d9d2f78ac71d22102ed6f
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-05 23:10:30 -0500

    return ever new list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    la_list = my_list
+    la_list = my_list.copy()
     for i in my_list:
         continue
     if idx < 0 or i <= idx:
