6c7cc10776e319954ad5f395c1b58566a5fb44f6
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-05 23:36:12 -0500

    conditional

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return len(sentence), sentence[0]
-
+    if sentence == 0:
+        return len(sentence), None
+    else:
+        return len(sentence), sentence[0]
