8293e25cdede1065c3aeb1c2536f9aee13717819
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-06 00:01:44 -0500

    first

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,8 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     else:
-        my_list.sort()
-        for i in my_list:
-            continue
-        return i
+        first = my_list[0]
+        for i in my_list[1:]:
+            if i > first:
+                first = i
+        return first
