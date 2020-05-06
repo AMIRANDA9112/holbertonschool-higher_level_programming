71cb6e37567bd34e2221633b98391e7a804c6385
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-05 23:48:23 -0500

    max number

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    m = 0
+    for i in my_list:
+        continue
+    if i == 0:
+        return None
+    else:
+        my_list.sort()
+        return my_list[i]
