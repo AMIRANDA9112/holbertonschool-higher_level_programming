544341363082cb587f806eaf79aa6622cc3c9009
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-07 22:35:42 -0500

    i how position or index and not how pointer

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace,):
+def search_replace(my_list, search, replace):
     """
 
+    :param replace: new element
+    :param search: element to replace
     :type my_list: index on list
     """
-    for i in my_list:
-        if my_list[i] == search:
-            la_list = my_list.copy()
+    la_list = my_list.copy()  # type: list
+    for i in range (len(la_list)):
+        if la_list[i] == search:
             la_list[i] = replace
             return la_list
-
-
