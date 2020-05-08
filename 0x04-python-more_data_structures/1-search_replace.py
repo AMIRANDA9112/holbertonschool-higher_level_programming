5a58b79378f24480491c14ad918c69161d0f7f23
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-07 22:37:25 -0500

    whitespaces

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -7,7 +7,7 @@ def search_replace(my_list, search, replace):
     :type my_list: index on list
     """
     la_list = my_list.copy()  # type: list
-    for i in range (len(la_list)):
+    for i in range(len(la_list)):
         if la_list[i] == search:
             la_list[i] = replace
             return la_list
