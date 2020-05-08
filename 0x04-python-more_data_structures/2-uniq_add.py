6831a0a65d7791851df56fa16e312d1600d019de
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-07 23:20:48 -0500

    add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    for i in my_list:
-        i, l = l, i
-        continue
-    return i
+    ion = 0
+    for i in set(my_list):
+        ion += i
+    return ion
