adb4ffc75d052879a41475adb7e319ee1123f2af
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-19 00:16:07 -0500

    /n

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
+def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
-        print("{%d}".format(value))
-        return True
-    except:
-        return False
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+        print()
+    except IndexError:
+        print("")
+    return count
