42a073eba992235d15d2dcf26544cd00a963b9f5
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-19 23:34:49 -0500

    other valor other init, and isinstance check

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""this module create class Square"""
+
+
+class Square():
+    """Square class"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """ Verifying type of value, size and position"""
+
+        self.size = size
+        try:
+            self.position = position
+        except TypeError as err:
+            print(err)
+
+    @property
+    def position(self):
+        """ Return the position """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (type(value) != tuple or len(value) != 2 or
+                not isinstance(value[0], int) or
+                not isinstance(value[1], int) or
+                value[0] < 0 or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    @property
+    def size(self):
+        """to return size of Square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter size of square"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return square of size how area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """ Print a square with # """
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                print("#" * self.size)
+
