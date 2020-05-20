5da130e5068d1450aefe77ced3f3fc91a371bc07
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-19 22:32:30 -0500

    private atribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this module create class Square"""
+
+
+class Square():
+    """Square class"""
+    def __init__(self, size):
+        """size: size of square"""
+        self.__size = size
