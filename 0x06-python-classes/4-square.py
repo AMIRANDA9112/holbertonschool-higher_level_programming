142cd8258dec6e41ed536f430ed6a72f9b2b0360
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-19 23:08:57 -0500

    setter an getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""this module create class Square"""
+
+
+class Square():
+    """Square class"""
+    def __init__(self, size=0):
+        """size: size of square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        """Function area(size)"""
+
+    def area(self):
+        """return square of size how area"""
+        return self.__size**2
+
+    @property
+    def size(self):
+        """to return size of Square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter size of square"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
