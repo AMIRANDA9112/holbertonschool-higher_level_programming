ddfba0fc32b0fe52ce85c12c289bfbbfc5f2a4f3
AMIRANDA9112 <andreselfm@gmail.com>
2020-05-25 23:39:16 -0500

    square

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -119,6 +119,8 @@ class Rectangle():
 
     @classmethod
     def square(cls, size=0):
-
-        """instance that return a rectangle with the same size how width and height "square" """
+        """
+        instance that return a rectangle with the same
+        size how width and height "square"
+        """
         return cls(size, size)
