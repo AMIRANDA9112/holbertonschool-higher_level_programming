b531c97240e388b3d5758b804969df528992243e
AMIRANDA9112 <andreselfm@gmail.com>
2020-06-06 23:03:16 -0500

    raise typerror

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,8 +3,11 @@
 
 
 def add_attribute(obj, name, value):
-    """
-    add attributes to a object
 
-    """
+
+
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    elif hasattr(obj, "__slots__") and not hasattr(obj, name):
+        raise TypeError("can't add new attribute")
     setattr(obj, name, value)
