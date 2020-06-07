381d6276469e71be47b32fdbff31cb4a0006b293
AMIRANDA9112 <andreselfm@gmail.com>
2020-06-06 21:23:02 -0500

    sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module return a sorted list"""
+
+
+class MyList(list):
+    """
+        this class list
+        """
+
+    def print_sorted(self):
+        """
+
+                :return: sorted list
+                """
+        print(sorted(self))
