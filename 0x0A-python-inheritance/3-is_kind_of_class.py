ab3480fcae1782dd87e3c6c2414dc5a33ca503af
AMIRANDA9112 <andreselfm@gmail.com>
2020-06-06 22:14:30 -0500

    kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module check the kind of class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+
+                :param obj: to compare
+                :param a_class: to compare
+        """
+    return isinstance(obj, a_class)
