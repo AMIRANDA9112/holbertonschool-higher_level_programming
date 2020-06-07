e348312f4d2cbad1696bc80bc712797d5234e432
AMIRANDA9112 <andreselfm@gmail.com>
2020-06-06 22:24:02 -0500

    base gemometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module creates a class"""
+
+
+class BaseGeometry():
+    """
+    BaseGeometry superclass
+    """
+
+    def area(self):
+        """
+
+            :param self:
+        """
+        raise Exception("area() is not implemented")
