293e37ae124f621cc58753b2e6bfdd3fc0637cc4
AMIRANDA9112 <andreselfm@gmail.com>
2020-06-09 16:53:42 -0500

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,79 @@
+# !/usr/bin/python3
+"""
+This module create a rectangle class
+from base class
+"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Class Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Class constructor"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ Getter """
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """ Setter """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        """ Getter """
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """ Setter """
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
+
+    @property
+    def x(self):
+        """ Getter  """
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """ Setter """
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
+
+    @property
+    def y(self):
+        """ Getter """
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """ Setter """
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
+
+    def area(self):
+        """ area function """
+        return self.__width * self.__height
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Unittest """
+import unittest
+import json
+import sys
+import io
+
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    """Tests class"""
+
+    def setUp(self):
+        """base at start point"""
+        Base._Base__nb_objects = 0
+
+    def test00(self):
+        """
+        this tests check that each
+        class have a unique id
+
+        base class = p0
+        id = 0
+        """
+        p1 = Base()
+        p2 = Base()
+        self.assertEqual(p1.id, 1)
+        self.assertEqual(p2.id, 2)
+
+    def test01(self):
+        """
+        This Test check
+        the correct set of id
+        """
+        b_0 = Base(9)
+        self.assertEqual(b_0.id, 9)
+
+    def test01(self):
+        """
+        This Test check
+        the correct set of id
+        """
+        b_0 = Base("text")
+        self.assertEqual(b_0.id, "text")
+
+
+
+
