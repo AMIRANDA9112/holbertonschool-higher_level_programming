47cbdb29979b4ae5d11e3b4511794ae1e950f0ce
AMIRANDA9112 <andreselfm@gmail.com>
2020-08-09 21:41:18 -0500

    list

---
+++ 0x0F-python-object_relational_mapping /0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" list a item """
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+
+    user = argv[1]
+    password = argv[2]
+    mydb = argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=my_pass,
+        db=my_db)
+
+    cursor = db.cursor()
+    database = cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for i in range(database):
+        print(cursor.fetchone())
+    db.close()
