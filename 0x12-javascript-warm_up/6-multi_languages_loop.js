b27d5399d487aad78d971cbf4efc8606a5e489a9
AMIRANDA9112 <andreselfm@gmail.com>
2020-09-16 21:18:27 -0500

    welcome to js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const l = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-for (let index = 0; index < languages.length; index++) {
-  console.log(languages[index]);
+for (let index = 0; index < l.length; index++) {
+  console.log(l[index]);
 }
