c5e35ff0ba78583e37aac823294ac57f230695cd
AMIRANDA9112 <andreselfm@gmail.com>
2020-09-16 22:13:44 -0500

    welcome to js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,5 +8,5 @@ const number = argv.length;
 if (number <= 3) {
   console.log('0');
 } else {
-  const se = argv.sort(function (a, b) { return (b - a); });
+  const se = argv.sort(function (a, b) { return (b - a);});
   console.log(se[3]);}
