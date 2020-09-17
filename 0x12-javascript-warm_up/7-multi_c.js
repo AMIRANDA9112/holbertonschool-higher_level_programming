b82e6594bb9a20164bda813e090ef95877f95f28
AMIRANDA9112 <andreselfm@gmail.com>
2020-09-16 21:19:41 -0500

    welcome to js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,10 @@
 
 const times = parseInt(process.argv[2]);
 
-if (isNaN(times)) {
+if (!isNaN(times)) {
+  for (let i = 0; i < times; i++) {
+    console.log('C is fun');
+  }
+} else {
   console.log('Missing number of occurrences');
-
-  } else {
-    for (let i = 0; i < times; i++) {
-    console.log('C is fun');}}
+}
