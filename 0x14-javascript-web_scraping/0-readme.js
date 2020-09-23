1d844738fe2256230b5292709b3b6c74bd5cacd3
AMIRANDA9112 <andreselfm@gmail.com>
2020-09-22 21:33:36 -0500

    first try

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const fileSyste = require('fs');
-fileSystes.readFile(process.argv[2], 'utf8', function (err, contents) {
+const fileSystem = require('fs');
+fileSystem.readFile(process.argv[2], 'utf8', function (err, contents) {
   if (contents === undefined) {
     console.log(err);
   } else {
